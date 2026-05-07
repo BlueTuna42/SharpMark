273d08548d7838df5b796e8c34054c47fbf18632
BlueTuna <dizim833@gmail.com>
2026-05-07 15:55:30 +0300

    Add deb packet builder and nixOS support
    Added nixOS support and deb packet builder

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "SharpMark: Fast Image Sharpness Analyzer and Culling Tool";
+
+  inputs = {
+    # Using the unstable branch for the latest GTK and LibRaw versions
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Supported systems for the desktop application
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      
+      # Helper function to generate an attrset for all systems
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      
+      # Nixpkgs instantiated for each system
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      # The main package (buildable with 'nix build')
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "sharpmark";
+            version = "1.0.0";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkg-config
+              wrapGAppsHook # Required for GTK apps to handle icons/schemas
+            ];
+
+            buildInputs = with pkgs; [
+              gtk3
+              libraw
+            ];
+
+            cmakeFlags = [
+              "-DCMAKE_BUILD_TYPE=Release"
+            ];
+
+            meta = with pkgs.lib; {
+              description = "Fast Image Sharpness Analyzer and Culling Tool";
+              license = licenses.asl20;
+              platforms = platforms.linux;
+              mainProgram = "SharpMark";
+            };
+          };
+        });
+
+      # Development shell (active with 'nix develop')
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            # Includes dependencies needed for local development and compilation
+            buildInputs = with pkgs; [
+              cmake
+              pkg-config
+              gtk3
+              libraw
+              gdb
+            ];
+          };
+        });
+    };
+}
